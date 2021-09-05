8f90c741ac24051fc7cd1a1ef1a0b7dd2037dc55
artempolcreator <artempolienko123@gmail.com>
2021-09-05 13:04:54 +0300

    hw 9 fully done

--- hw/hw9.py
+++ hw/hw9.py
@@ -1,3 +1,5 @@
+from functools import reduce
+
 # Решить несколько задач из projecteuler.net
 #
 # Решения должны быть максимально лаконичными, и использовать list comprehensions.
@@ -7,7 +9,7 @@
 # problem48 - list comprehension : one line
 # problem40 - list comprehension
 
-# # problem9
+# problem9
 print(
     [(a, b, c) for a in range(1000) for b in range(a) for c in range(b) if a * a == b * b + c * c if a + b + c == 1000])
 
@@ -18,4 +20,8 @@ print(sum([a for a in range(100)])**2 - sum([a*a for a in range(100)]))
 print(str(sum([a ** a for a in range(1001)]))[-10:])
 
 # problem40
-
+digits_list = [str(a) for a in range(1, 199999)]
+digits_str = "".join(digits_list)
+result_list = map(int, [digits_str[9], digits_str[99], digits_str[999],
+                        digits_str[9999], digits_str[99999], digits_str[999999]])
+print(reduce((lambda x, y: x * y), result_list))
