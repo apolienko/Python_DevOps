64eb6ddc6d5a46980223b7f576b62f5ea1850813
artempolcreator <artempolienko123@gmail.com>
2021-09-20 13:46:07 +0300

    hw 7 final

--- hw/hw7/hw7.py
+++ hw/hw7/hw7.py
@@ -13,7 +13,7 @@ groups = {}
 name_to_UID = {}
 
 shell_index = 6
-group_index = 4
+group_index = 3
 uid_index = 2
 name_index = 0
 
@@ -22,27 +22,35 @@ with open('passwd.txt', 'r') as file:
         fields = line.rstrip().split(":")
         shells[fields[shell_index]] = shells.get(fields[shell_index], 0) + 1
         name_to_UID[fields[name_index]] = fields[uid_index]
+        if fields[group_index] in groups.keys():
+            groups[fields[group_index]] = groups[fields[group_index]] + "," + fields[uid_index]
+        else:
+            groups[fields[group_index]] = fields[uid_index]
 
-group_index = 0
+group_name = 0
+group_index = 2
 
 with open('group.txt', 'r') as file:
     for line in file:
         fields = line.rstrip().split(":")
-        if fields[group_index] in name_to_UID:
-            groups[fields[group_index]] = name_to_UID[fields[group_index]]
+        if fields[group_index] in groups:
+            groups[fields[group_name]] = groups.get(fields[group_index])
+            del groups[fields[group_index]]
+        else:
+            groups[fields[group_name]] = ''
         if fields[3] != '':
             users = fields[3].split(",")
             for user in users:
                 uid = name_to_UID[user]
-                if fields[group_index] in groups:
-                    groups[fields[group_index]] = groups[fields[0]] + "," + uid
+                if fields[group_name] in groups:
+                    groups[fields[group_name]] = groups[fields[group_name]] + "," + uid
                 else:
                     groups[fields[group_index]] = uid
 
 with open('output.txt', 'w') as file:
     for sh in shells:
-        file.write("%s - %d ; " % (sh, shells[sh]))
-    file.write("\n")
+        file.write("%s - %d ;" % (sh, shells[sh]))
+    file.write("\n\n")
     for g in groups:
-        file.write("%s:%s ; " % (g, groups[g]))
+        file.write("%s:%s ;\n" % (g, groups[g]))
 
